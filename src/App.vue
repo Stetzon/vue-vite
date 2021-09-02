c9d084bd87f27a7917b3f8cb6ea508b6bf5b7e7d
Stetson Lewis <stetson.lewis@outlook.com>
2021-09-02 11:43:38 -0400

    add all the providers

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
-<template>  
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <router-view></router-view>
+<template>
+  <n-loading-bar-provider>
+    <n-dialog-provider>
+      <n-notification-provider>
+        <n-message-provider>
+          <img alt="Vue logo" src="./assets/logo.png" />
+          <router-view></router-view>
+        </n-message-provider>
+      </n-notification-provider>
+    </n-dialog-provider>
+  </n-loading-bar-provider>
 </template>
 
 <style>
