5d3377d690fafdce8806454b0617f27cf3e3f4ce
Stetson Lewis <stetson.lewis@outlook.com>
2021-09-01 21:15:30 -0400

    add naive and fonts to app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import naive from 'naive-ui'
 import App from './App.vue'
+import 'vfonts/Lato.css'
+// Monospace Font
+// import 'vfonts/FiraCode.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(naive)
+    .mount('#app')
